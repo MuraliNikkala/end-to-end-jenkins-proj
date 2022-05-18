fd5209c1c69568aacb00e8a1814935760a0ac7ad
MuraliNikkala <104074775+MuraliNikkala@users.noreply.github.com>
2022-05-18 13:04:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,11 @@
 provider "aws" {
-    access_key = "AKIA2QEFLENWCZ57K3KB"
-    secret_key = "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"
     region = "us-east-1"
 }
 
 data "aws_ami" "my_ami" {
      most_recent      = true
      #name_regex       = "^mavrick"
-     owners           = ["721834156908"]
+     owners           = ["747068662246"]
 }
 
 
@@ -16,10 +14,10 @@ resource "aws_instance" "web-1" {
     #ami = "ami-0d857ff0f5fc4e03b"
     availability_zone = "us-east-1a"
     instance_type = "t2.micro"
-    key_name = "LaptopKey"
-    subnet_id = "subnet-01f9c2774277f4015"
-	private_ip = "10.1.1.111"
-    vpc_security_group_ids = ["sg-09c2fc6ae1a93c80e"]
+    key_name = "NVWEBSERVER2-NIT-Key"
+    subnet_id = "subnet-06bd1be4d3a6e47de"
+	#private_ip = "10.1.1.111"
+    vpc_security_group_ids = ["sg-084b42dc4c9669219"]
     associate_public_ip_address = true	
     tags = {
         Name = "Server-1"
